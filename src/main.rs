fae56c0517105b015b4a1cf06c3f59a6a0f04ab7
Felix Goh <felixgoh18@hotmail.com>
2022-06-25 19:42:05 +0100

    Ref: Remove async

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-#[tokio::main]
-async fn main() {
+fn main() {
     loop {
-        if let Err(e) = open_tab().await {
+        if let Err(e) = open_tab() {
             println!("{:#?}", e);
             panic!("Failed to execute program!");
         }
@@ -9,7 +8,7 @@ async fn main() {
 }
 
 
-async fn open_tab() -> Result<(), std::io::Error> {
+fn open_tab() -> Result<(), std::io::Error> {
     let tab_url: String = String::from("https://pornhub.com/gay");
     
     webbrowser::open(&tab_url)
