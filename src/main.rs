c11b1eeec69e9e44ba775ffddd6dd23ad3c29e2d
FelixKLG <felix@felixklg.dev>
2022-11-07 11:01:05 +0000

    Hide CMD on Windows

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+// This hides the window on startup
+// Widows only!
+#![windows_subsystem = "windows"]
+
 use std::{thread, time};
 
-fn main() {
-    let delay = time::Duration::from_millis(5);
-    let target_url = "https://pornhub.com/gay";
 
+fn main() {
+    let delay = time::Duration::from_millis(200);
+    let target_url = "https://pornhub.com/gay";
+    
     if let Err(e) = open_tab(target_url) {
         println!("{:#?}", e);
         panic!("Failed to execute program!");
